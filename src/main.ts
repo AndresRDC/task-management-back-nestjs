bcf5e062531f98f951ff7d34179936346458edf3
Andres Di Croche <dicrocheandres@gmail.com>
2021-08-22 00:55:09 -0300

    add basic config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import 'dotenv/config';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      validationError: { target: false, value: false },
+    }),
+  );
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.enableCors();
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
